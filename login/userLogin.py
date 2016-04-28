e3cdad06e506019d23d8a88c451e58ffb85f8beb
Narasimman <narasi.mit@gmail.com>
2016-04-28 02:13:36 -0400

    few nitpick changes to user registration

--- login/userLogin.py
+++ login/userLogin.py
@@ -1,6 +1,6 @@
 from flask import Flask
 from flask.ext.sqlalchemy import SQLAlchemy
-from flask import Flask,session, request, flash, url_for, redirect, render_template, abort ,g
+from flask import Flask, session, request, flash, url_for, redirect, render_template, abort ,g, jsonify
 from flask.ext.login import LoginManager, login_user , logout_user , current_user , login_required
 from werkzeug.security import generate_password_hash, check_password_hash
 from datetime import datetime
@@ -20,23 +20,23 @@ def load_user(id):
 
 class User(db.Model):
     __tablename__ = "users"
-    id = db.Column('user_id',db.Integer , primary_key=True)
-    username = db.Column('username', db.String(100))
+    id = db.Column('user_id', db.Integer, primary_key=True)
+    username = db.Column('username', db.String(32), index = True)
     password = db.Column('password' , db.String(128))
     email = db.Column('email',db.String(100))
     firstname = db.Column('firstname',db.String(100)) 
-    middlename = db.Column('secondname',db.String(100))
+    middlename = db.Column('middlename',db.String(100))
     lastname = db.Column('lastname',db.String(100))
     phoneNumber = db.Column('phoneNumber',db.Integer)
     registered_on = db.Column('registered_on' , db.DateTime)
  
-    def __init__(self , username ,password , email):
+    def __init__(self, username, password, email, firstname, lastname):
         self.username = username
         self.set_password(password)
         self.email = email
-	#self.firstname = firstname
+	self.firstname = firstname
 	#self.middlename = middlename
-	#self.lastname = lastname
+	self.lastname = lastname
 	#self.phoneNumber = phoneNumber
         self.registered_on = datetime.utcnow()
     
@@ -62,41 +62,51 @@ class User(db.Model):
         return '<User %r>' % (self.username)
  
 
-@app.route('/register' , methods=['GET','POST'])
+@app.route('/register' , methods=['GET', 'POST'])
 def register():
     if request.method == 'GET':
         return render_template('register.html')
-    user = User(request.form['username'] , request.form['password'],request.form['email'])
+ 
+    username  = request.form['username']
+    password  = request.form['password']
+    email     = request.form['email']
+    #firstname = request.form['firstname'] 
+    #lastname  = request.form['lastname']
+    firstname = ""
+    lastname = ""
+    user = User(username, password, email, firstname, lastname)
     db.session.add(user)
     db.session.commit()
+    
     flash('User successfully registered')
-    return redirect(url_for('login'))
+    #return redirect(url_for('login'))
+    return jsonify({ 'username': user.username }), 201, {'Location': url_for('login', id = user.id, _external = True)}
 
-@app.route('/login',methods=['GET','POST'])
+@app.route('/login',methods=['GET', 'POST'])
 def login():
+
     if request.method == 'GET':
         return render_template('login.html')
+
     username = request.form['username']
     password = request.form['password']
+
     registered_user = User.query.filter_by(username=username).first()
     flash(registered_user.check_password(registered_user.password))
+
     if registered_user and registered_user.check_password(registered_user.check_password):
 	login_user(registered_user)
 	flash('Logged in successfully')
 	return ('{"%s":"success"}'%username)
     else:
-   # if registered_user is None:
         flash('Username or Password is invalid' , 'error')
         return ('{"%s":"failure"}'%username)
-        return redirect(url_for('login'))
-   # login_user(registered_user)
-   # flash('Logged in successfully')
-   # return ('{%s,success}'%username)
+        #return redirect(url_for('login'))
 
 @app.route('/landing',methods=['GET','POST'])
 def landing():
-	if request.method == 'GET':
-		return render_template('landing.html')
+    if request.method == 'GET':
+        return render_template('landing.html')
 		
 if __name__ == '__main__':
-	app.run(host='0.0.0.0', port=7002,debug=True)
+    app.run(host='0.0.0.0', port=7002,debug=True)
